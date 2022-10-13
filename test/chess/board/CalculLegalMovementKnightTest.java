1670fcb2ec87df162721b503643dac37dea91c2f
abdessettar <abdinos64@gmail.com>
2022-10-13 22:21:06 +0200

    CalculLegalMovementRookTest et reverification de methode checkmate

--- test/chess/board/CalculLegalMovementKnightTest.java
+++ test/chess/board/CalculLegalMovementKnightTest.java
@@ -1,12 +1,14 @@
 package chess.board;
 
 import chess.ChessGame;
+import chess.Movement.AttackMovement;
 import chess.Movement.Movement;
 import chess.chessPiece.*;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CalculLegalMovementKnightTest {
@@ -32,6 +34,7 @@ public class CalculLegalMovementKnightTest {
 
 
     }
+
     @Test
     void testLegalNormalFriendlyOccupiedPositionMovement() {
         boardGame = new BoardGame(chessGame);
@@ -45,8 +48,104 @@ public class CalculLegalMovementKnightTest {
         }
         ArrayList<Integer> expectedPositions = new ArrayList<>();
         expectedPositions.add(11);
-       expectedPositions.add(18);
+        expectedPositions.add(18);
         assertTrue(expectedPositions.size() == actualPositions.size() && actualPositions.containsAll(expectedPositions));
+    }
+
+    @Test
+    void testFirstColumnExclusionMovement() {
+        boardGame = new BoardGame(chessGame);
+        boardGame.createBoard();
+        boardGame.getBoard().put(40, new Knight(40, PieceColor.WHITE, new CalculLegalMovementKnight()));
+        ArrayList<Movement> actualMovements = (ArrayList<Movement>) boardGame.getChessPieceAtPosition(40).findLegalMovements(boardGame, false);
+        ArrayList<Integer> actualPositions = new ArrayList<>();
+        for (Movement movement : actualMovements) {
+            actualPositions.add(movement.getFuturePosition());
+        }
+        ArrayList<Integer> expectedPositions = new ArrayList<>();
+        expectedPositions.add(25);
+        expectedPositions.add(34);
+        expectedPositions.add(50);
+        expectedPositions.add(57);
+        assertTrue(actualPositions.size() == expectedPositions.size() && actualPositions.containsAll(expectedPositions));
+
+    }
+    @Test
+    void testSecondColumnExclusionMovement() {
+        boardGame = new BoardGame(chessGame);
+        boardGame.createBoard();
+        boardGame.getBoard().put(41, new Knight(41, PieceColor.WHITE, new CalculLegalMovementKnight()));
+        ArrayList<Movement> actualMovements = (ArrayList<Movement>) boardGame.getChessPieceAtPosition(41).findLegalMovements(boardGame, false);
+        ArrayList<Integer> actualPositions = new ArrayList<>();
+        for (Movement movement : actualMovements) {
+            actualPositions.add(movement.getFuturePosition());
+        }
+        ArrayList<Integer> expectedPositions = new ArrayList<>();
+        expectedPositions.add(35);
+        expectedPositions.add(58);
+        expectedPositions.add(51);
+        expectedPositions.add(26);
+         expectedPositions.add(24);
+         expectedPositions.add(56);
+        assertTrue(actualPositions.size() == expectedPositions.size() && actualPositions.containsAll(expectedPositions));
+
+    }
+    @Test
+    void testSeventhColumnExclusionMovement() {
+        boardGame = new BoardGame(chessGame);
+        boardGame.createBoard();
+        boardGame.getBoard().put(46, new Knight(46, PieceColor.WHITE, new CalculLegalMovementKnight()));
+        ArrayList<Movement> actualMovements = (ArrayList<Movement>) boardGame.getChessPieceAtPosition(46).findLegalMovements(boardGame, false);
+        ArrayList<Integer> actualPositions = new ArrayList<>();
+        for (Movement movement : actualMovements) {
+            actualPositions.add(movement.getFuturePosition());
+        }
+        ArrayList<Integer> expectedPositions = new ArrayList<>();
+        expectedPositions.add(31);
+        expectedPositions.add(29);
+        expectedPositions.add(61);
+        expectedPositions.add(63);
+        expectedPositions.add(52);
+        expectedPositions.add(36);
+        assertTrue(actualPositions.size() == expectedPositions.size() && actualPositions.containsAll(expectedPositions));
+
+    }
+    @Test
+    void testEighthColumnExclusionMovement() {
+        boardGame = new BoardGame(chessGame);
+        boardGame.createBoard();
+        boardGame.getBoard().put(47, new Knight(47, PieceColor.WHITE, new CalculLegalMovementKnight()));
+        ArrayList<Movement> actualMovements = (ArrayList<Movement>) boardGame.getChessPieceAtPosition(47).findLegalMovements(boardGame, false);
+        ArrayList<Integer> actualPositions = new ArrayList<>();
+        for (Movement movement : actualMovements) {
+            actualPositions.add(movement.getFuturePosition());
+        }
+        ArrayList<Integer> expectedPositions = new ArrayList<>();
+        expectedPositions.add(30);
+        expectedPositions.add(62);
+        expectedPositions.add(53);
+        expectedPositions.add(37);
+        assertTrue(actualPositions.size() == expectedPositions.size() && actualPositions.containsAll(expectedPositions));
+
+    }
+    @Test
+    void testLegalNormalEnemyOccupiedPositionMovement() {
+        boardGame = new BoardGame(chessGame);
+        boardGame.createBoard();
+        ChessPiece enemyBlackPawn = new Pawn(41, PieceColor.BLACK, new CalculLegalMovementPawn());
+        boardGame.getBoard().put(58, new Knight(58, PieceColor.WHITE, new CalculLegalMovementKnight()));
+        boardGame.getBoard().put(41, enemyBlackPawn);
+        ArrayList<Movement> actualMovements = (ArrayList<Movement>) boardGame.getChessPieceAtPosition(58).findLegalMovements(boardGame, true);
+        AttackMovement attackMovement = null;
+        for (Movement movement : actualMovements) {
+            System.out.println(movement.getFuturePosition());
+           /* if (movement.getClass() == AttackMovement.class)
+                attackMovement = (AttackMovement) movement;
+            System.out.println(attackMovement.getFuturePosition());*/
+        }
+        //assertTrue(attackMovement != null && attackMovement.getFuturePosition() == 41);
+
+
     }
 
 }
---
+++ test/chess/board/CalculLegalMovementRookTest.java
@@ -0,0 +1,261 @@
+package chess.board;
+
+import chess.ChessGame;
+import chess.Movement.AttackCheckMovement;
+import chess.Movement.AttackMovement;
+import chess.Movement.Movement;
+import chess.chessPiece.*;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class CalculLegalMovementRookTest {
+
+    BoardGame boardGame;
+    ChessGame chessGame;
+
+    @Test
+    void testLegalNormalFreeMovement() {
+        boardGame = new BoardGame(chessGame);
+        boardGame.createBoard();
+        boardGame.getBoard().put(17, new Rook(17, PieceColor.BLACK, new CalculLegalMovementRook()));
+        ArrayList<Movement> actualMovements = (ArrayList<Movement>) boardGame.getChessPieceAtPosition(17).findLegalMovements(boardGame, true);
+        ArrayList<Integer> actualPositions = new ArrayList<>();
+        for (Movement movement : actualMovements) {
+            actualPositions.add(movement.getFuturePosition());
+        }
+        ArrayList<Integer> expectedPositions = new ArrayList<>();
+        expectedPositions.add(23);
+        expectedPositions.add(16);
+        expectedPositions.add(19);
+        expectedPositions.add(18);
+        expectedPositions.add(20);
+        expectedPositions.add(21);
+        expectedPositions.add(22);
+        expectedPositions.add(1);
+        expectedPositions.add(9);
+        expectedPositions.add(25);
+        expectedPositions.add(33);
+        expectedPositions.add(41);
+        expectedPositions.add(49);
+        expectedPositions.add(57);
+
+        assertTrue(expectedPositions.size() == actualPositions.size() && actualPositions.containsAll(expectedPositions));
+
+
+    }
+    @Test
+    void testLegalNormalFriendlyOccupiedPositionMovement() {
+        boardGame = new BoardGame(chessGame);
+        boardGame.createBoard();
+        boardGame.getBoard().put(25,new Pawn(25,PieceColor.BLACK,new CalculLegalMovementPawn()));
+        boardGame.getBoard().put(17, new Rook(17, PieceColor.BLACK, new CalculLegalMovementRook()));
+        ArrayList<Movement> actualMovements = (ArrayList<Movement>) boardGame.getChessPieceAtPosition(17).findLegalMovements(boardGame, false);
+        ArrayList<Integer> actualPositions = new ArrayList<>();
+        for (Movement movement : actualMovements) {
+            actualPositions.add(movement.getFuturePosition());
+        }
+        ArrayList<Integer> expectedPositions = new ArrayList<>();
+        expectedPositions.add(23);
+        expectedPositions.add(16);
+        expectedPositions.add(19);
+        expectedPositions.add(18);
+        expectedPositions.add(20);
+        expectedPositions.add(21);
+        expectedPositions.add(22);
+        expectedPositions.add(1);
+        expectedPositions.add(9);
+        assertTrue(expectedPositions.size() == actualPositions.size() && actualPositions.containsAll(expectedPositions));
+
+
+    }
+
+    @Test
+    void testFirstColumnExclusionMovement() {
+        boardGame = new BoardGame(chessGame);
+        boardGame.createBoard();
+        boardGame.getBoard().put(0, new Rook(0, PieceColor.BLACK, new CalculLegalMovementRook()));
+        ArrayList<Movement> actualMovements = (ArrayList<Movement>) boardGame.getChessPieceAtPosition(0).findLegalMovements(boardGame, true);
+        ArrayList<Integer> actualPositions = new ArrayList<>();
+        for (Movement movement : actualMovements) {
+            actualPositions.add(movement.getFuturePosition());
+        }
+        ArrayList<Integer> expectedPositions = new ArrayList<>();
+        expectedPositions.add(1);
+        expectedPositions.add(2);
+        expectedPositions.add(3);
+        expectedPositions.add(4);
+        expectedPositions.add(5);
+        expectedPositions.add(6);
+        expectedPositions.add(7);
+        expectedPositions.add(8);
+        expectedPositions.add(16);
+        expectedPositions.add(24);
+        expectedPositions.add(32);
+        expectedPositions.add(40);
+        expectedPositions.add(48);
+        expectedPositions.add(56);
+        assertTrue(expectedPositions.size() == actualPositions.size() && actualPositions.containsAll(expectedPositions));
+    }
+
+    @Test
+    void testEightColumnExclusionMovement() {
+        boardGame = new BoardGame(chessGame);
+        boardGame.createBoard();
+        boardGame.getBoard().put(0, new Rook(7, PieceColor.BLACK, new CalculLegalMovementRook()));
+        ArrayList<Movement> actualMovements = (ArrayList<Movement>) boardGame.getChessPieceAtPosition(0).findLegalMovements(boardGame, false);
+        ArrayList<Integer> actualPositions = new ArrayList<>();
+        for (Movement movement : actualMovements) {
+            actualPositions.add(movement.getFuturePosition());
+        }
+        ArrayList<Integer> expectedPositions = new ArrayList<>();
+        expectedPositions.add(1);
+        expectedPositions.add(2);
+        expectedPositions.add(3);
+        expectedPositions.add(4);
+        expectedPositions.add(5);
+        expectedPositions.add(6);
+        expectedPositions.add(15);
+        expectedPositions.add(23);
+        expectedPositions.add(31);
+        expectedPositions.add(39);
+        expectedPositions.add(47);
+        expectedPositions.add(55);
+        expectedPositions.add(63);
+        assertTrue(expectedPositions.size() == actualPositions.size() && actualPositions.containsAll(expectedPositions));
+    }
+    @Test
+    void testLegalNormalEnemyOccupiedPositionMovement() {
+        boardGame = new BoardGame(chessGame);
+        boardGame.createBoard();
+        ChessPiece enemyWhitePawn = new Pawn(8, PieceColor.WHITE, new CalculLegalMovementPawn());
+        boardGame.getBoard().put(0, new Bishop(0, PieceColor.BLACK, new CalculLegalMovementRook()));
+        boardGame.getBoard().put(8, enemyWhitePawn);
+        ArrayList<Movement> actualMovements = (ArrayList<Movement>) boardGame.getChessPieceAtPosition(0).findLegalMovements(boardGame, true);
+        AttackMovement attackMovement = null;
+        for (Movement movement : actualMovements) {
+            if (movement.getClass() == AttackMovement.class)
+                attackMovement = (AttackMovement) movement;
+        }
+        assertTrue(attackMovement != null && attackMovement.getFuturePosition() == 8);
+    }
+    @Test
+    void testLegalNormalEnemyOccupiedPositionMovementWhite() {
+        boardGame = new BoardGame(chessGame);
+        boardGame.createBoard();
+        ChessPiece enemyWhitePawn = new Pawn(55, PieceColor.BLACK, new CalculLegalMovementPawn());
+        boardGame.getBoard().put(63, new Bishop(63, PieceColor.WHITE, new CalculLegalMovementRook()));
+        boardGame.getBoard().put(55, enemyWhitePawn);
+        ArrayList<Movement> actualMovements = (ArrayList<Movement>) boardGame.getChessPieceAtPosition(63).findLegalMovements(boardGame, false);
+        AttackMovement attackMovement = null;
+        for (Movement movement : actualMovements) {
+            if (movement.getClass() == AttackMovement.class)
+                attackMovement = (AttackMovement) movement;
+        }
+        assertTrue(attackMovement != null && attackMovement.getFuturePosition() == 55);
+    }
+    @Test
+    void testLegalCheckmateFreePositionMovement() {
+        boardGame = new BoardGame(chessGame);
+        boardGame.createBoard();
+        ChessPiece rook2 = new Rook(57, PieceColor.BLACK, new CalculLegalMovementRook());
+        ChessPiece enemyKing = new King(16, PieceColor.WHITE, new CalculLegalMovementKing());
+        ChessPiece rook = new Rook(1, PieceColor.BLACK, new CalculLegalMovementRook());
+        boardGame.getBoard().put(57, rook2);
+        boardGame.getBoard().put(16, enemyKing);
+        boardGame.getBoard().put(1, rook);
+        ArrayList<Movement> actualMovements = (ArrayList<Movement>) boardGame.getChessPieceAtPosition(1).findLegalMovements(boardGame, true);
+        AttackCheckMovement attackCheckMovement = null;
+        for (Movement movement : actualMovements) {
+            if (movement.getClass() == AttackCheckMovement.class)
+               attackCheckMovement = (AttackCheckMovement) movement;
+            System.out.println(movement.getFuturePosition());
+        }
+        assertTrue(attackCheckMovement != null && attackCheckMovement.getFuturePosition() == 0);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
