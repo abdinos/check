d60cb3b01305aa8421aedb5bef8f66c29baac53b
loic fery <loic-fery@orange.fr>
2022-10-14 22:47:58 +0200

    update et correction

--- src/chess/ChessGame.java
+++ src/chess/ChessGame.java
@@ -8,6 +8,7 @@ import chess.chessPiece.Pawn;
 import chess.chessPiece.PieceColor;
 import chess.gui.BoardGameGUI;
 import chess.gui.ChessGameMainWindow;
+import chess.gui.MainWindow;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -22,32 +23,11 @@ public class ChessGame {
 
     private BoardGame boardGame;
 
-    private BoardGameGUI boardGameGUI;
-
     private ChessGameMainWindow chessGameMainWindow;
-
-    private boolean isEndGame;
-
-    private boolean isDrawn;
 
     public ChessGame() {
         this.boardGame = new BoardGame(this);
         boardGame.setCalculKingCheck(new StandardCalculKingCheck(boardGame));
-
-        //test
-        //MainWindow mainWindow = new MainWindow();
-        //mainWindow.setVisible(true);
-        /**
-        try{
-            MainWindow mainWindow = new MainWindow();
-            this.boardGameGUI = new BoardGameGUI(boardGame.getBoard(),mainWindow.getJFrame());
-        }
-        catch (IOException e){
-            e.printStackTrace();
-        }
-         **/
-        isEndGame = false;
-        isDrawn = false;
     }
 
     /**
@@ -86,10 +66,6 @@ public class ChessGame {
 
     public List<Player> getPlayers(){
         return players;
-    }
-
-    public boolean isEndGame(){
-        return isEndGame;
     }
 
     /**
@@ -134,11 +110,9 @@ public class ChessGame {
 
             if(boardGame.isDraw()){
                 chessGameMainWindow.draw();
-                isDrawn = true;
             }
             else if (boardGame.isGameEnded(currentPlayer.getPlayerColor(), enemyPlayer)) {
                     chessGameMainWindow.endGame();
-                    isEndGame = true;
                 }
             else{
                 indexCurrentPlayer = (indexCurrentPlayer + 1) % players.size();
@@ -156,96 +130,6 @@ public class ChessGame {
         return boardGame.getChessPieceLegalMovements(chessPiece);
     }
 
-    /**
-     * interface de test sur console
-     * TO DELETE
-     */
-    public void interfaceTest(){
-        Scanner scanner = new Scanner(System.in);
-        String response = "";
-        int position = -1;
-        int tour = 1;
-
-        System.out.println("Début de la partie \n");
-
-        while(!isEndGame && !isDrawn){
-            do {
-                System.out.println(boardGame + "\n");
-
-                System.out.println("tour " + tour);
-                System.out.println("Au tour de " + players.get(indexCurrentPlayer).getName() + "\n");
-
-                System.out.println("choisit la pièce à bouger (position) :");
-                response = scanner.next();
-                try {
-                    position = Integer.parseInt(response);
-                }
-                catch (Exception e){}
-            }while(position < 0 || position > 63);
-
-            // Get chess piece at the position enter
-            ChessPiece chessPiece = boardGame.getBoard().get(position);
-            if(chessPiece != null) {
-                System.out.println("Mouvement de " + chessPiece.chessPieceToString() + " : ");
-                int index = 1;
-                List<Movement> movements = new ArrayList<>();
-                if (boardGame.getChessPieceLegalMovements(chessPiece) != null) {
-                    movements = boardGame.getChessPieceLegalMovements(chessPiece).stream().toList();
-
-                    for (Movement movement : movements) {
-                        System.out.print(index++ + " : " + movement.getChessPieceMoved().getPiecePosition() + " --> " + movement.getFuturePosition());
-                        if (movement.isAttacking()) {
-                            System.out.print(" (" + movement.getChessPieceAttacked().chessPieceToString() + ")");
-                        }
-                        System.out.println(" / " + movement.getClass());
-                    }
-                    System.out.println();
-
-                    do {
-                        System.out.println("choisir le mouvement ou changer de piece (new) \n");
-                        response = scanner.next();
-
-                        if (!response.equals("new")) {
-                            try {
-                                position = Integer.parseInt(response) - 1;
-                                if (position >= movements.size() || position < 0) {
-                                    System.out.println("valeur incorrect ! \n");
-                                } else {
-                                    response = "move";
-                                }
-                            } catch (Exception e) {
-                            }
-                        }
-                    } while (!response.equals("new") && !response.equals("move"));
-                } else {
-                    System.out.println("Aucune mouvement possible \n");
-                    response = "";
-                }
-
-                if (response.equals("move")) {
-                    Movement movement = movements.get(position);
-                    executeChessPieceMovement(movement);
-
-                    Player enemyPlayer = players.get(indexCurrentPlayer);
-                    if(enemyPlayer.isKingCheck()){
-                        System.out.println(enemyPlayer.getName() + " : check state\n");
-                    }
-
-                    if(!isEndGame) {
-                        System.out.println("changement de joueur \n");
-                        tour++;
-                    }
-                }
-            }
-        }
-        if(isEndGame) {
-            System.out.println("\nVictoire de " + players.get((indexCurrentPlayer + 1) % players.size()).getName());
-        }
-        if(isDrawn){
-            System.out.println("Match nul");
-        }
-    }
-
     public void setChessGameMainWindow(ChessGameMainWindow chessGameMainWindow){
         if(chessGameMainWindow != null){
             this.chessGameMainWindow = chessGameMainWindow;
@@ -255,15 +139,7 @@ public class ChessGame {
     public static void main(String[] args) {
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
             public void run() {
-                ChessGame chessGame = new ChessGame();
-                chessGame.createPlayers();
-                chessGame.initChessGame();
-
-                //chessGame.interfaceTest();
-
-                ChessGameMainWindow chessGameMainWindow = new ChessGameMainWindow(chessGame);
-                chessGame.setChessGameMainWindow(chessGameMainWindow);
-                chessGameMainWindow.createAndShoPlayMenu();
+                new MainWindow().setVisible(true);
             }
         });
     }
--- src/chess/gui/MainWindow.java
+++ src/chess/gui/MainWindow.java
@@ -17,11 +17,12 @@ public class MainWindow extends JFrame {
     private ChessWindow chessWindow;
     private BoardGameGUI boardGameGUI = null;
 
-    private JFrame jFrame = this;;
+    private JFrame jFrame = this;
+    ;
 
-    public MainWindow(){
+    public MainWindow() {
         setTitle("Jeu d'échecs"); // titre de l'interface graphique
-        setSize(800,1200); // dimension de l'interface 1200x800
+        setSize(800, 1200); // dimension de l'interface 1200x800
         setResizable(false); // la fenetre ne sera pas redimensionnable par l'utilisateur
         setDefaultCloseOperation(EXIT_ON_CLOSE); // fermeture de l'interface une fois que l'utilisateur click sur (X)
         setLayout(null); // pour chacun des éléments graphiques il faudra donner les coord. (x,y) & la largeur et la hauteur
@@ -30,100 +31,65 @@ public class MainWindow extends JFrame {
         // btn JOUER
         //setLayout( new GridBagLayout() );
         JButton playBtn = new JButton("JOUER");
-        playBtn.setBounds(320,100, 200,40);
+        playBtn.setBounds(300, 700, 200, 40);
         play(playBtn);
         add(playBtn);
 
         // btn COMMENT JOUER
         JButton howBtn = new JButton("COMMENT JOUER");
-        howBtn.setBounds(320,150,200,40);
+        howBtn.setBounds(300, 750, 200, 40);
         howToPlay(howBtn);
         add(howBtn);
 
         // btn QUITTER
         JButton leaveBtn = new JButton("QUITTER");
-        leaveBtn.setBounds(320,200,200,40);
+        leaveBtn.setBounds(300, 800, 200, 40);
         leaveTheGame(leaveBtn);
         add(leaveBtn);
 
         //bg
         JLabel background;
-        ImageIcon imageIcon = new ImageIcon("images/mainbg.jpg");
-        background = new JLabel("",imageIcon,JLabel.HORIZONTAL);
-        background.setBounds(0,0,800,900);
+        ImageIcon imageIcon = new ImageIcon("images/bg.jpg");
+        background = new JLabel("", imageIcon, JLabel.HORIZONTAL);
+        background.setBounds(0, 0, 800, 900);
         add(background);
 
         //chessGame = new ChessGame();
     }
 
 
-
-    public void play(JButton jButton){
-        jButton.addActionListener( new ActionListener()
-        {
-            public void actionPerformed(ActionEvent e)
-            {
-                try {
-                    boardGame = new BoardGame(chessGame);
-                    boardGame.createBoard();
-                    boardGame.initChessPieceOnBoard();
-                    chessWindow = new ChessWindow();
-                    dispose();
-
-                    boardGameGUI = new BoardGameGUI(boardGame.getBoard());
-                }
-                catch (IOException ex) {
-                    throw new RuntimeException(ex);
-                }
-            }
-        });
-    }
-    public void howToPlay(JButton jButton){
-        jButton.addActionListener( new ActionListener()
-        {
-            public void actionPerformed(ActionEvent e)
-            {
-                //TODO : une fenetre qui montre comment jouer
-            }
-        });
-    }
-
-    public void leaveTheGame(JButton jButton){
-        jButton.addActionListener( new ActionListener()
-        {
-            public void actionPerformed(ActionEvent e)
-            {
+    public void play(JButton jButton) {
+        jButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                ChessGame chessGame = new ChessGame();
+                chessGame.createPlayers();
+                chessGame.initChessGame();
+                ChessGameMainWindow chessGameMainWindow = new ChessGameMainWindow(chessGame);
+                chessGame.setChessGameMainWindow(chessGameMainWindow);
+                chessGameMainWindow.createAndShoPlayMenu();
                 dispose();
             }
         });
     }
 
-    public JFrame getJFrame(){
-        return this;
-    }
-
-    /**
-    public static void main(String[] args) {
-        EventQueue.invokeLater(new Runnable(){
-            @Override
-            public void run() {
-                MainWindow mainWindow = new MainWindow();
-                UIManager.put("swing.boldMetal", Boolean.FALSE);
-                //ChessGame chessGame = null;
-                //chessGame = new ChessGame();
-                //BoardGame boardGame = new BoardGame(chessGame);
-                //chessGame.initChessGame();
-                //boardGame.createBoard();
-                //boardGame.initChessPieceOnBoard();
-                //BoardGameGUI boardGameGUI = null;
-                try {
-                    boardGameGUI = new BoardGameGUI(boardGame.getBoard(),mainWindow.chessWindow.getJFrame());
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-                mainWindow.setVisible(true);
+    public void howToPlay(JButton jButton) {
+        jButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                new LearnChessWindow().setVisible(true);
+                dispose();
             }
         });
     }
-    **/
+
+    public void leaveTheGame(JButton jButton) {
+        jButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+            }
+        });
+    }
+
+    public JFrame getJFrame() {
+        return this;
+    }
 }
